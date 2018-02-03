d53192d6f93f26d1dd5826526a229efe7c46342f
Nemolite <g25092011@mail.ru>
2018-02-03 14:21:39 +0300

    menu

--- js/script.js
+++ js/script.js
@@ -4,3 +4,14 @@ $(document).ready(function(){
     autoplaySpeed:2000
   });
 });
+
+// меню
+
+$(document).ready(function(){
+   
+   $("#navs2").click(function(){$("#navs2-inner").toggle(1000)});
+    $("#navs4").click(function(){$("#navs4-inner").toggle(1000)});
+    $("#navs8").click(function(){$("#navs8-inner").toggle(1000)});
+   
+
+});
